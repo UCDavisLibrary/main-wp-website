8ce8ac2ac6a7bd80a4c01087cdc918114eefc3c2
Sabrina Baggett <sabrinabaggett@administrators-MacBook-Pro.local>
2025-11-12 14:25:38 -0800

    requested changes #4

--- utils/wp-scripts/assign-research-highlights.php
+++ utils/wp-scripts/assign-research-highlights.php
@@ -1,11 +1,24 @@
 <?php
 if ( defined('WP_CLI') && WP_CLI ) {
   $inCLI = true;
+} else {
+  $inCLI = false;
 }
+
 $dry_run = false; 
 
+if ( $inCLI && isset( $GLOBALS['argv'] ) ) {
+  foreach ( $GLOBALS['argv'] as $arg ) {
+    if ( $arg === 'dry-run' || $arg === 'dry_run' ) {
+      $dry_run = true;
+      break;
+    } 
+  }
+}
+
+
 $people = $GLOBALS['UCDLibPluginDirectory']->people;
-$people->ensureBlockExists($dry_run, $inCLI );
+$people->ensureResearchHighlightBlockExists($dry_run, $inCLI );
 
 
   
